e5288520e453b4aad36be088a72f60e99d948318
newchayanon <new.chayanon20054@gmail.com>
2024-07-20 01:24:08 +0700

    edit : home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { StockContextProvider } from "../contexts/StockContext";
 import ImageSlidesShow from "../layouts/ImageSlidesShow";
@@ -11,39 +12,36 @@ import { useUser } from "../hooks/useUser";
 export default function HomePage() {
   const { setIsUser } = useUser();
   const [newProduct, setNewProduct] = useState();
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        setLoading(true);
+        // setLoading(true);
         const query = new URLSearchParams(window.location.search);
         const tokenFromUrl = query.get("token");
-
-        const resNewProduct = await stockApi.getNewProduct();
-        setNewProduct(resNewProduct.data.fetch);
 
         if (tokenFromUrl) {
           const base64Token = decodeURIComponent(tokenFromUrl);
           setAccessToken(base64Token);
-
-          const res = await userApi.getUser();
-          setIsUser(res.data.user);
+          const [resUser, resNewProduct] = await Promise.all([userApi.getUser(), stockApi.getNewProduct()]);
+          setIsUser(resUser.data.user);
+          setNewProduct(resNewProduct.data.fetch);
           window.history.replaceState({}, document.title, "/");
+        } else {
+          const resNewProduct = await stockApi.getNewProduct();
+          setNewProduct(resNewProduct.data.fetch);
         }
       } catch (error) {
         console.log(error);
-      } finally {
-        setLoading(false);
       }
     };
     fetchUser();
-  }, [setIsUser]);
+  }, []);
 
   return (
     <div>
-      {loading && <Spinner />}
       <StockContextProvider>
+        {!newProduct && <Spinner />}
         <ImageSlidesShow />
         <ProductRecommend title="สินค้าใหม่" newProduct={newProduct} />
         <ProductRecommend title="สินค้าขายดี (coming soon)" newProduct={newProduct} />

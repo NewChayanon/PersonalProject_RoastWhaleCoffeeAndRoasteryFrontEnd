aaca3df3d8362b82f4c2cbc9f9d7235c2bfecf1b
newchayanon <new.chayanon20054@gmail.com>
2024-08-22 03:07:17 +0700

    edit : code

--- src/layouts/PaymentZoom.jsx
+++ src/layouts/PaymentZoom.jsx
@@ -3,7 +3,7 @@
 export default function PaymentZoom({ image }) {
   return (
     <div className="flex justify-center items-center">
-      <img src={`http://localhost:8888/${image}`} alt="payment" className="rounded-md" />
+      <img src={image} alt="payment" className="rounded-md" />
     </div>
   );
 }
